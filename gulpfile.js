21baf973a42893c93e93dbd4a0e8f5c7d61eb20e
win <win@pixnet.tw>
2017-10-14 00:07:40 +0800

    add modules folder to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -22,18 +22,30 @@ gulp.task('default', ['minify-css', 'minify-js'], () => {
 });
 
 gulp.task('purify-css', () => {
-    return gulp.src('./public/css/**/*.css')
+     gulp.src('./public/modules/gallery/css/*.css')
+    .pipe(purify(['./public/modules/gallery/js/*.js', './public/**/*.html']))
+    .pipe(gulp.dest('./public/modules/gallery/css/'));
+
+     gulp.src('./public/css/**/*.css')
     .pipe(purify(['./public/js/**/*.js', './public/**/*.html']))
     .pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('minify-css', ['purify-css'], () => {
-    return gulp.src('./public/css/**/*.css')
+     gulp.src('./public/modules/gallery/css/*.css')
+    .pipe(cleanCSS({compatibility: '*'}))
+    .pipe(gulp.dest('./public/modules/gallery/css'));
+
+     gulp.src('./public/css/**/*.css')
     .pipe(cleanCSS({compatibility: '*'}))
     .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('minify-js', () => {
+    gulp.src('./public/modules/gallery/js/**/*.js')
+    .pipe(minifyjs())
+    .pipe(gulp.dest('./public/modules/gallery/js'));
+
     gulp.src('./public/js/**/*.js')
     .pipe(minifyjs())
     .pipe(gulp.dest('./public/js'));
